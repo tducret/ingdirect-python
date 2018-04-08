166529ae92ee54877cb6fd8fc8b9624a27b8efd4
thibdct <thibault.ducret@gmail.com>
2018-04-08 17:46:49 +0200

    Modifications pour simplifier les nouveaux projets

--- setup.py
+++ setup.py
@@ -2,24 +2,33 @@
 from setuptools import setup
 from pip.req import parse_requirements
 
+# Basé sur http://peterdowns.com/posts/first-time-with-pypi.html
+
 _VERSION = '0.0.1'
+_NOM_PACKAGE = 'ingdirect'
+_URL_GITHUB = 'https://github.com/tducret/ingdirect-python'
+_DESCRIPTION = 'Package pour consulter ses comptes bancaires ING Direct'
+_MOTS_CLES=['api', 'banque', 'ING', 'ingdirect', 'comptes']
+
+
 install_reqs = parse_requirements('requirements.txt', session='hack')
 requirements = [str(ir.req) for ir in install_reqs]
 
 setup(
-  name = 'ingdirect',
-  packages = ['ingdirect'],
+  name = _NOM_PACKAGE,
+  packages = [_NOM_PACKAGE],
   version = _VERSION,
   license='MIT',
   platforms='Posix; MacOS X',
-  description = 'Package pour consulter ses comptes bancaires ING Direct',
+  description = _DESCRIPTION,
   author = 'Thibault Ducret',
   author_email = 'thibault.ducret@gmail.com',
-  url = 'https://github.com/tducret/ingdirect-python',
-  download_url = 'https://github.com/tducret/bibliotheque-toulouse-python/tarball/%s' % (_VERSION), # I'll explain this in a second
-  keywords = ['api', 'banque', 'ING', 'ingdirect', 'comptes'],
+  url = _URL_GITHUB,
+  download_url = '%s/tarball/%s' % (_URL_GITHUB, _VERSION),
+  keywords = _MOTS_CLES,
   setup_requires=requirements,
   install_requires=requirements,
-  classifiers = ['Programming Language :: Python :: 3.6'],
+  classifiers = ['Programming Language :: Python :: 3'],
+  python_requires='>=3',
   tests_require=['pytest'],
 )
