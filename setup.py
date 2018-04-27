a7c8944cc218e3bba6d7d91dac924c22768516da
thibdct <thibault.ducret@gmail.com>
2018-04-27 23:01:25 +0200

    Correction pour les versions de pip > 10

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
-from pip.req import parse_requirements
+try:  # Pour pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # Pour pip <= 9.0.3
+    from pip.req import parse_requirements
 from ingdirect import __version__
 
 # Basé sur http://peterdowns.com/posts/first-time-with-pypi.html
@@ -20,25 +23,25 @@ install_reqs = parse_requirements('requirements.txt', session='hack')
 requirements = [str(ir.req) for ir in install_reqs]
 
 setup(
-  name=_NOM_PACKAGE,
-  packages=[_NOM_PACKAGE],
-  package_data={_NOM_PACKAGE: _PACKAGE_DATA, },
-  scripts=_SCRIPTS,
-  version=__version__,
-  license='MIT',
-  platforms='Posix; MacOS X',
-  description=_DESCRIPTION,
-  long_description=_DESCRIPTION,
-  author='Thibault Ducret',
-  author_email='thibault.ducret@gmail.com',
-  url=_URL_GITHUB,
-  download_url='%s/tarball/%s' % (_URL_GITHUB, __version__),
-  keywords=_MOTS_CLES,
-  setup_requires=requirements,
-  install_requires=requirements,
-  classifiers=['Programming Language :: Python :: 3'],
-  python_requires='>=3',
-  tests_require=['pytest'],
+    name=_NOM_PACKAGE,
+    packages=[_NOM_PACKAGE],
+    package_data={_NOM_PACKAGE: _PACKAGE_DATA, },
+    scripts=_SCRIPTS,
+    version=__version__,
+    license='MIT',
+    platforms='Posix; MacOS X',
+    description=_DESCRIPTION,
+    long_description=_DESCRIPTION,
+    author='Thibault Ducret',
+    author_email='thibault.ducret@gmail.com',
+    url=_URL_GITHUB,
+    download_url='%s/tarball/%s' % (_URL_GITHUB, __version__),
+    keywords=_MOTS_CLES,
+    setup_requires=requirements,
+    install_requires=requirements,
+    classifiers=['Programming Language :: Python :: 3'],
+    python_requires='>=3',
+    tests_require=['pytest'],
 )
 
 # ------------------------------------------
