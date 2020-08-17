6efd300902411a1af1bea2d3c71fbef7122d1a34
Thibault Ducret <thibault.ducret@gmail.com>
2020-08-17 22:08:48 +0200

    Fix #2: modification setup.py pour la récupération des requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
 from setuptools import setup
-try:  # Pour pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # Pour pip <= 9.0.3
-    from pip.req import parse_requirements
+
+HERE = Path(__file__).parent
+reqs_path = HERE / 'requirements.txt'
+with open(reqs_path) as reqs_file:
+    requirements = reqs_file.read().splitlines()
 
 # Basé sur http://peterdowns.com/posts/first-time-with-pypi.html
 
-__version__ = '0.1.1'  # En cohérence avec __init.py__
+__version__ = '0.1.2'  # En cohérence avec __init.py__
 _NOM_PACKAGE = 'ingdirect'
 _URL_GITHUB = 'https://github.com/tducret/ingdirect-python'
 _DESCRIPTION = 'Package pour consulter ses comptes bancaires ING Direct'
@@ -18,9 +20,6 @@ _PACKAGE_DATA = ['images_chiffres_keypad/*.png']
 _SCRIPTS = ['ing.py']
 # A supprimer ici + 'scripts' dans setup()
 # si aucune commande exécutable n'est utilisée par le package
-
-install_reqs = parse_requirements('requirements.txt', session='hack')
-requirements = [str(ir.req) for ir in install_reqs]
 
 setup(
     name=_NOM_PACKAGE,
@@ -49,7 +48,7 @@ setup(
 # ------------------------------------------
 # S'assurer que tout a été commité et pushé via git status
 # (sinon git commit --am "Commentaire" et git push)
-# export VERSION=0.1.1; git tag $VERSION -m "Changement de casse du header Ingdf-Auth-Token"; git push --tags
+# export VERSION=0.1.2; git tag $VERSION -m "Fix #2: modification setup.py pour la récupération des requirements"; git push --tags
 
 # Pour effacer un tag
 # git push --delete origin $VERSION; git tag -d $VERSION
